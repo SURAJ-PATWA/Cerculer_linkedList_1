47418649f2b6a28915cdb5ef2995a63607ef9509
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2022-08-25 23:45:58 +0530

    Add files via upload
    Traverse A Single Linked List start to end

---
+++ Treverse_A_Circular_LinkedList_.java
@@ -0,0 +1,33 @@
+public class Treverse_A_Circular_LinkedList_ {
+    static class Node{
+        int data;
+        Node next;
+        Node(int x){
+            data=x;
+            next=null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Node head=new Node(10);
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+       head.next.next.next=head;  // it show Circuler Linked List  // yaha per circular linkedList create ho gya haa
+        printlist(head);
+
+    }
+
+    public static void printlist(Node head){
+      if(head==null) return ;
+        System.out.print(head.data+" ");// first element ko pahele he print karna haa
+        for(Node curr=head.next;curr!=head;curr=curr.next){
+            System.out.print(curr.data+" ");
+        }
+    }
+
+
+
+
+
+}
